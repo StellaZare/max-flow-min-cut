7ebc25b7dd1ea134db0a3b3fb1c3b8a33d5f1ac3
Stella Zarei <zareistella@gmail.com>
2023-08-03 13:36:45 -0700

    adds test cases for max min function

--- app/test_maxmin.cpp
+++ app/test_maxmin.cpp
@@ -120,3 +120,41 @@ TEMPLATE_TEST_CASE("inputs/graph_4 test case", "[interval]", int, double){
 
     CHECK(result == expected); 
 }
+
+TEMPLATE_TEST_CASE("inputs/graph_1 test case src=1 sink=5", "[interval]", int, double){
+    std::size_t graph_size = 6;
+    TestType expected = 8;
+    
+    DirWeightedGraph<TestType> graph {graph_size};
+
+    std::istringstream input {
+        "0 7 0 0 4 0 0 0 5 3 0 0 0 0 0 0 0 8 0 0 3 0 0 5 0 3 0 2 0 0 0 0 0 0 0 0"
+    };
+
+    graph.read_adjacency_matrix(input);
+    
+    std::vector<int> path;
+    path.resize(graph_size);
+    TestType result = maxmin::max_flow_min_cut(1, graph_size-1, graph);
+
+    CHECK(result == expected); 
+}
+
+TEMPLATE_TEST_CASE("inputs/graph_1 test case src=0 sink=2", "[interval]", int, double){
+    std::size_t graph_size = 6;
+    TestType expected = 8;
+    
+    DirWeightedGraph<TestType> graph {graph_size};
+
+    std::istringstream input {
+        "0 7 0 0 4 0 0 0 5 3 0 0 0 0 0 0 0 8 0 0 3 0 0 5 0 3 0 2 0 0 0 0 0 0 0 0"
+    };
+
+    graph.read_adjacency_matrix(input);
+    
+    std::vector<int> path;
+    path.resize(graph_size);
+    TestType result = maxmin::max_flow_min_cut(0, 2, graph);
+
+    CHECK(result == expected); 
+}
